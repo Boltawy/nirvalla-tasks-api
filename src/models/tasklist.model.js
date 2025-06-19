445b10eeaa205cc00ed054702697f777d3d6fac5
Bolt4Action <109246542+Bolt4Action@users.noreply.github.com>
2025-06-19 13:54:33 +0300

    dtiio

--- src/models/tasklist.model.js
+++ src/models/tasklist.model.js
@@ -1,12 +1,10 @@
 import mongoose, { model, Schema } from "mongoose";
 
-const taskSchema = new Schema({
-    title: { type: String, required: true, maxLength: 40 },
+const taskListSchema = new Schema({
+    title: { type: String, maxLength: 40, required: true },
     description: { type: String, maxLength: 200 },
     user: { type: ObjectId, ref: 'User', required: true },
-
-
 }, { timestamps: true })
 
-const Task = mongoose.models.Task || model("Task", taskSchema)
-export default Task 
+const TaskList = mongoose.models.TaskList || model("TaskList", taskListSchema)
+export default TaskList 

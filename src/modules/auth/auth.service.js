08ace6d9f072b72e2879e83efabc9bff69dc97c0
Bolt4Action <Zeyadisma3eel@gmail.com>
2025-06-22 16:50:20 +0300

    used dbSafeUtils in authservice

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -2,33 +2,19 @@ import userModel from "../../models/user.model.js";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import { responseError } from "../../utils/errorHandler.js";
-import { safeFindOne } from "../../utils/dbSafeUtils.js";
+import { safeCreate, safeFindOne } from "../../utils/dbSafeUtils.js";
 
-//DONE: a try/catch wrapper around DB operations 
 
 const authService = {
     signUp: async (userName, email, password) => {
-        const user = await safeFindOne(userModel, { email }, "User already exists")
-        if (user) {
-            throw new responseError(409, "User already exists")
-        }
+        const user = await safeFindOne(userModel, { email }, { errOnNotFound: false })
+        if (user) throw new responseError(409, "User already exists")
         const hashedPassword = await bcrypt.hash(password, 8)
-
-        try {
-            await userModel.insertOne({ userName, email, password: hashedPassword }, { validateBeforeSave: true })
-        } catch (error) {
-            console.log(error)
-            if (error.message.includes("validation failed")) {
-                throw new responseError(400, "Validation Error")
-            }
-            else throw new responseError(520, "Unknown error occured")
-        }
+        await safeCreate(userModel, { userName, email, password: hashedPassword })
     },
+
     login: async (loginEmail, loginPassword) => {
-        const result = await safeFindOne(userModel, { email: loginEmail }, "Error fetching user"); //Invalid Email, Or other server errors.
-        if (!result) {
-            throw new responseError(404, "Invalid credentials") //User not found
-        }
+        const result = await safeFindOne(userModel, { email: loginEmail },); //Invalid Email, Or other server errors.
         const { _id, userName, password } = result
         const isValidPassword = await bcrypt.compare(loginPassword, password)
         if (!isValidPassword) {

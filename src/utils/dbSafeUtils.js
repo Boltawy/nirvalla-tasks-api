b0efb7d104782753b9f6b1570de387fa12e45b7c
Bolt4Action <Zeyadisma3eel@gmail.com>
2025-06-23 17:19:07 +0300

    added findByIdAndVerifyOwner() & sort for SafeFind()

--- src/utils/dbSafeUtils.js
+++ src/utils/dbSafeUtils.js
@@ -1,5 +1,4 @@
 import mongoose, { mongo } from "mongoose";
-import userModel from "../models/user.model.js";
 import { responseError } from "./errorHandler.js";
 //TODO Options object for all functions
 
@@ -48,36 +47,54 @@ export const safeFindById = async (model, id, options = {}) => {
     let result;
     try {
         result = await model.findById(id, projection);
-        if (!result & errOnNotFound) throw new Error();
+        if (!result & errOnNotFound) throw new responseError(404, `${modelName} not found`);
         return result;
     } catch (error) {
-        if (!result & errOnNotFound) throw new responseError(404, `${modelName} not found`);
+        if (error instanceof responseError) throw error;
         else throw new responseError(500, errMessage || `Error reading ${modelName} from database`, error);
     }
 };
+
+/**
+ * Finds a document by its ID and verifies that the user has access to it.
+ * @param {mongoose.Model} model - The Mongoose model to perform the operation on.
+ * @param {string} itemId - The id of the document to find.
+ * @param {string} userId - The id of the user to verify access for.
+ * @returns {Promise<mongoose.Document>} The document if the user has access.
+ * @throws {responseError} If the user does not have access, or if the operation fails.
+ */
+export const findByIdAndVerifyOwner = async (model, itemId, userId) => { //Not Abstract
+    if (!mongoose.Types.ObjectId.isValid(userId)) throw new responseError(400, `Invalid ID format for userId`);
+    const item = await safeFindById(model, itemId);
+    if (item.userId != userId) throw new responseError(403, "Unauthorized: You don't have access to that resource");
+    return item;
+}
 
 
 
 /**
  * Safely performs a find operation on a Mongoose model.
  * @param {mongoose.Model} model - The Mongoose model to perform the operation on.
- * @param {Object} data - The filter to apply to find the documents.
+ * @param {Object} filter - The filter to apply to find the documents.
  * @param {Object} [options] - Options for the operation.
  * @param {string} [options.errMessage] - The error message to use if the operation fails.
+ * @param {boolean} [options.errOnNotFound=false] - Whether to throw an error if no documents are found.
  * @param {Object} [options.projection] - The projection to apply to the found documents.
+ * @param {Object} [options.sort] - The sort order to apply to the found documents.
  * @returns {Promise<mongoose.Document[]>} The result of the operation.
  * @throws {responseError} If the operation fails.
  */
-export const safeFind = async (model, data, options = {}) => {
-    const { errMessage, errOnNotFound, projection = {} } = options;
+
+export const safeFind = async (model, filter, options = {}) => {
+    const { errMessage, errOnNotFound, projection = {}, sort = null } = options;
     const { modelName } = model;
     let result;
     try {
-        result = await model.find(data, projection);
-        if (result.length == 0 && errOnNotFound) throw new Error();
+        result = await model.find(filter, projection).sort(sort);
+        if (result.length == 0 && errOnNotFound) throw new responseError(404, `No ${modelName}s found`);
         return result;
     } catch (error) {
-        if (result.length == 0 && errOnNotFound) throw new responseError(404, `No ${modelName}s found`);
+        if (error instanceof responseError) throw error;
         throw new responseError(500, errMessage || `Error reading ${modelName}s from database`, error);
     }
 };
@@ -112,7 +129,7 @@ export const safeCreate = async (model, data, errMessage) => {
 export const safeUpdate = async (model, filter, updateData, errMessage) => {
     const { modelName } = model;
     try {
-        const result = await model.updateOne(filter, updateData);
+        const result = await model.updateOne(filter, updateData, { new: true, runValidators: true });
         return result;
     } catch (error) {
         throw new responseError(500, errMessage || `Error updating ${modelName} in database`, error);
@@ -136,6 +153,7 @@ export const safeUpdateById = async (model, id, updateData, errMessage) => {
         if (!result) throw new responseError(404, `${modelName} not found`);
         return result;
     } catch (error) {
+        if (error instanceof responseError) throw error;
         throw new responseError(500, errMessage || `Error updating ${modelName} in database`, error);
     }
 };

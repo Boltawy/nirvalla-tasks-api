f98766787b4c49589558ac40118ad0ef2d262da9
Bolt4Action <Zeyadisma3eel@gmail.com>
2025-07-21 17:38:56 +0300

    added baseUrl of the frontend

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,4 +1,5 @@
 import authService from "./auth.service.js";
+
 import successHandler from "../../utils/successHandler.js";
 import { compareSync } from "bcrypt";
 
@@ -19,10 +20,10 @@ const authController = {
         const email = emails?.[0].value;
         const avatar = photos?.[0].value;
         if (!email || !avatar) { //TODO Handle if any data is missing, Create error oAuth page in frontend.
-            return res.redirect(`http://localhost:3000`)
+            return res.redirect(`${process.env.FRONTEND_URL}`)
         }
         const { accessToken, refreshToken } = await authService.signUpOrLogin(displayName, email, avatar);
-        res.redirect(`http://localhost:3000/google-redirect?accessToken=${accessToken}&refreshToken=${refreshToken}`)
+        res.redirect(`${process.env.FRONTEND_URL}/google-redirect?accessToken=${accessToken}&refreshToken=${refreshToken}`)
     },
     githubAuth: async (req, res) => {
         const { displayName, emails, photos } = req.user;
@@ -30,10 +31,10 @@ const authController = {
         const email = emails?.[0].value;
         const avatar = photos?.[0].value;
         if (!email || !avatar) { //TODO Handle if any data is missing, Create error oAuth page in frontend.
-            return res.redirect(`http://localhost:3000`)
+            return res.redirect(`${process.env.FRONTEND_URL}`)
         }
         const { accessToken } = await authService.signUpOrLogin(displayName, email, avatar);
-        return res.redirect(`http://localhost:3000/github-redirect?accessToken=${accessToken}`)
+        return res.redirect(`${process.env.FRONTEND_URL}/github-redirect?accessToken=${accessToken}`)
     }
 }
 

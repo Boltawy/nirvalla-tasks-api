431953b89ec127032d0583b42a396dd3f47f0f3b
Bolt4Action <Zeyadisma3eel@gmail.com>
2025-07-23 17:26:47 +0300

    sync service now returns all tasks including completed by default

--- src/modules/sync/sync.service.js
+++ src/modules/sync/sync.service.js
@@ -57,7 +57,7 @@ const syncService = {
 
     generatePopulatedLists: async (userId) => {
         const tasklists = await safeFind(tasklistModel, { userId, deletedAt: null }, { sort: { order: 1 }, projection: { __v: 0 }, lean: true });
-        const tasks = await safeFind(taskModel, { userId, deletedAt: null, completedAt: null }, { sort: { order: 1 }, projection: { __v: 0 }, lean: true });
+        const tasks = await safeFind(taskModel, { userId, deletedAt: null }, { sort: { order: 1 }, projection: { __v: 0 }, lean: true });
         tasklists.forEach((list) => {
             Object.assign(list, { tasks: [] })
         })
